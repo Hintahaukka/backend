fe763bae8673ee588955d8c62c080595a1725415
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2019-09-05 22:14:20 +0300

    fixing

--- src/main/java/hintahaukka/App.java
+++ src/main/java/hintahaukka/App.java
@@ -7,16 +7,16 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-//        port(getHerokuAssignedPort());
+        port(getHerokuAssignedPort());
         
         get("/", (req, res) -> "Hello Heroku World");
     }
 
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
